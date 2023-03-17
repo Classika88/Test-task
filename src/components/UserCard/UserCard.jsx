289501655b4d557f5af1727a8684c3e6dd3d3206
Iryna Khamidova <waterholein@gmail.com>
2023-03-17 20:58:53 +0200

    Add method toLocaleString()

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -8,15 +8,21 @@ import  Line  from "../../img/Line.png";
 export class UserCard extends Component {
     state = {
         isClick: JSON.parse(localStorage.getItem('isClick')) ?? false,
+        followers: JSON.parse(localStorage.getItem('followers')) ?? 100500,
         }
     
     componentDidUpdate(_, prevState) {
         localStorage.setItem('isClick', JSON.stringify(this.state.isClick));
+        localStorage.setItem('followers', JSON.stringify(this.state.followers));
         }
-    onLeaveFeedback = event => {
-        event.preventDefault();
-        this.setState(prevState => ({isClick: !prevState.isClick}));
-        }; 
+    
+        onLeaveFeedback = event => {
+            event.preventDefault();
+            this.setState(prevState => ({
+              followers: prevState.isClick ? prevState.followers - 1 : prevState.followers + 1,
+              isClick: !prevState.isClick
+            }));
+          };
       render() {              
   return (
     <div className={css.container}>
@@ -25,7 +31,8 @@ export class UserCard extends Component {
         <img src={Boy} className={css.boy} alt="Boy"></img>
         <img src={Line} className={css.line} alt="Line"></img>
         <p className={css.tweets}>777 TWEETS</p>
-        <p className={css.followers}>{!this.state.isClick? '100,500' : '100,501'} FOLLOWERS</p>
+        <p className={css.followers}>
+            {this.state.followers.toLocaleString('en-US')} FOLLOWERS</p>
         <button type="button" onClick={this.onLeaveFeedback}
             className={!this.state.isClick ? css.button : css.buttonClicked}>
             {!this.state.isClick? 'FOLLOW' : 'FOLLOWING'}
